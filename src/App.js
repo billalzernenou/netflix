e529a63728ceff6b51db079691934e8f80488d93
Billal ZERNENOU <billalzernenou@gmail.com>
2021-02-02 17:49:19 +0100

    move map function to return statemens

--- src/App.js
+++ src/App.js
@@ -4,15 +4,16 @@ import Section from "./Section";
 import movies from "./movies.json";
 
 function App() {
-  // map movies data & call section component for each iteration
-  const tab = movies.map((element, index) => {
-    return <Section key={index} section={element} />;
-  });
   // return main component
+
   return (
     <div className="App">
       <Header />
-      <div className="sections">{tab}</div>
+      <div className="sections">
+        {movies.map((element, index) => {
+          return <Section key={index} section={element} />;
+        })}
+      </div>
     </div>
   );
 }
